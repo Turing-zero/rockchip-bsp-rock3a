48195ba33b599a8e35a99531e1e02b0c6ac20e06
Mark <mark_zjunlict@zju.edu.cn>
2022-05-24 21:23:09 +0800

    init

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#7 SMP Tue May 24 20:38:46 CST 2022"
+#define LINUX_COMPILE_BY "mark"
+#define LINUX_COMPILE_HOST "mark4ue"
+#define LINUX_COMPILER "aarch64-none-linux-gnu-gcc (GNU Toolchain for the Arm Architecture 11.2-2022.02 (arm-11.14)) 11.2.1 20220111, GNU ld (GNU Toolchain for the Arm Architecture 11.2-2022.02 (arm-11.14)) 2.37.20220122"
